bda90a3c08433d935b8e6336fcd77df8e182c91a
clucker357 <sr.taku3@gmail.com>
2022-10-09 23:44:38 +0000

    routing とコントローラーの作成

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ItemController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::resource('item', ItemController::class);
 
 Route::get('/', function () {
     return view('welcome');
