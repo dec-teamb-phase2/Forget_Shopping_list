d90c24a7674028024edb08c13a21332126586c20
Reia-O <ewokakunogasuki@gmail.com>
2022-10-10 21:20:49 +0900

    CREATE画面のCSS調整

--- resources/views/item/create.blade.php
+++ resources/views/item/create.blade.php
@@ -1,7 +1,9 @@
 <x-app-layout>
   <x-slot name="header">
     <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+      <center>
       {{ __('Create New Item') }}
+      </center>
     </h2>
   </x-slot>
 
