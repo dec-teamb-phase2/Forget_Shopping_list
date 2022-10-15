3cb240563df098bc8a9e1669cdc39ab5be4c4854
Reia-O <ewokakunogasuki@gmail.com>
2022-10-15 10:57:50 +0900

    CSS調整

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -49,7 +49,7 @@
                 @endif
 
                 <x-primary-button class="ml-3">
-                    {{ __('Log in') }}
+                    {{ __('ENTER') }}
                 </x-primary-button>
             </div>
         </form>
