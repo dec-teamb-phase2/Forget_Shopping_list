6c103b73beabc6ab06f17974c27d3532c96684b8
clucker357 <sr.taku3@gmail.com>
2022-10-15 02:21:14 +0000

    make deleteButton

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -100,7 +100,7 @@ class ItemController extends Controller
      */
     public function destroy($id)
     {
-        $result = Tweet::find($id)->delete();
-        return redirect()->route('tweet.index');
+        $result = Item::find($id)->delete();
+        return redirect()->route('item.index');
     }
 }
