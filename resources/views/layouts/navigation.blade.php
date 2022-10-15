7545823533004a2e10e8e76000c636f022b10ea1
Reia-O <ewokakunogasuki@gmail.com>
2022-10-15 10:55:22 +0900

    CSS調整

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,7 +13,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('Top Page') }}
                     </x-nav-link>
                 </div>
                 <!-- item 一覧画面 -->
@@ -76,7 +76,7 @@
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+                {{ __('Top Page') }}
             </x-responsive-nav-link>
         </div>
         <!--一覧ページへのリンク追加-->
